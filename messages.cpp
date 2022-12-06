5d07163c5e97ec9cc9e8edf380c508ad210cd31f
Alex3400 <63754699+Alex3400@users.noreply.github.com>
2022-12-06 15:05:24 +0000

    Update messages.cpp
    changed to display information about users messages
    changed emojis to vector<string, string> instead of the tuple because that wasn't working
    fixed bug where putting two empty lines would break code
    fixed bug where the messages for each user wouldn't update, only the overall messages vector would for multi lined messages
    added a bunch of statistic methods for the user and message classes.
    got rid of unnecessary methods at the top

--- messages.cpp
+++ messages.cpp
@@ -1,10 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <chrono>
-#include<tuple> 
+#include <tuple> 
 #include <algorithm>
 #include <cctype>
 #include <fcntl.h>
@@ -17,32 +17,14 @@
 
 using namespace std;
 
-unordered_map<string, tuple<string, int>> emojis;
+unordered_map<string, string> emojis;
 const string classification = u8"\U0000200D";
 
 
-// code taken from Bertrand Cachet on github to be able to write UTF-16 charachters to text files.
-std::wstring decode(const std::string& str, int codePage = GetACP())
-{
-    if (str.empty()) return std::wstring();
-    int size_needed = MultiByteToWideChar(codePage, 0, &str[0], (int)str.size(), NULL, 0);
-    std::wstring wstrTo(size_needed, 0);
-    MultiByteToWideChar(codePage, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-    return wstrTo;
-}
-
-std::string encode(const std::wstring& wstr, int codePage = GetACP())
-{
-    if (wstr.empty()) return std::string();
-    int size_needed = WideCharToMultiByte(codePage, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-    std::string strTo(size_needed, 0);
-    WideCharToMultiByte(codePage, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-    return strTo;
-} 
 
 
-unordered_map<string, tuple<string, int>> get_emojis() {
-    unordered_map<string, tuple<string, int>> e;
+unordered_map<string, string> get_emojis() {
+    unordered_map<string, string> e;
     fstream infile;
     infile.open("emojis-out.txt", ios::in);
     string tp;
@@ -50,12 +32,12 @@ unordered_map<string, tuple<string, int>> get_emojis() {
         size_t space = tp.find(" ");
         string emoji = tp.substr(0, space);
         string name = tp.substr(space);
-        e.insert({ emoji,  make_tuple(name + " ", 0) });
+        e.insert({ emoji,  name + " " });
     }
     infile.close();
-    e.insert({ u8"\U0000201c" , make_tuple("\"", 0) }); //iphones code start and end quotes, as well as apostrophes diffrently from how 
-    e.insert({ u8"\U0000201c" , make_tuple("\"", 0) }); //they're typically ascii coded so these 3 lines standardizes it.
-    e.insert({ u8"\U00002019",  make_tuple("'", 0) });
+    e.insert({ u8"\U0000201c" , "\""}); //iphones code start and end quotes, as well as apostrophes diffrently from how 
+    e.insert({ u8"\U0000201d" , "\""}); //they're typically ascii coded so these 3 lines standardizes it.
+    e.insert({ u8"\U00002019",  "'"});
     return e;
 }
 class Time {
@@ -151,21 +133,32 @@ class Message {
 private: 
     float avgWordLength;
     string content;
+    string name;
     Time time;
     vector<string> words;
     int chars;
+    int emojiCount;
 public:
     Message(){
         content = "";
         time = Time();
         chars = 0;
-        avgWordLength = 0;
+        avgWordLength = -1;
     }
     Message(string c, Time t){
         chars = 0;
         content = "";
         addContent(c);
         time = t;
+        avgWordLength = -1;
+    }
+    Message(string n, string c, Time t) {
+        chars = 0;
+        content = "";
+        addContent(c);
+        time = t;
+        avgWordLength = -1;
+        name = n;
     }
     void addContent(string c) {
         c = content + c;
@@ -175,8 +168,8 @@ public:
             if (c[i] < 0) { //emojis are denoted by negative charchters, so if a negative value is found search for emoji
                 int j = 1;
                 while (c[i + j] < 0) { //emojis can be of varying length so keep checking for negative chars
-                    tuple<string,int> e = emojis[c.substr(i, j + 1)]; //see if the string of negative chars corresponds to an emoji
-                    if (!get<0>(e).empty()) {                      //if it does:
+                    string e = emojis[c.substr(i, j + 1)]; //see if the string of negative chars corresponds to an emoji
+                    if (!e.empty()) {                      //if it does:
                         string s = c.substr(i + j + 1, 3);
                         if (s == classification) {//see if the 3 charachters after the emoji match up with the classification emoji
                             j += 2;               // if it does then the whole emoji is bigger and we should continue looking
@@ -184,19 +177,18 @@ public:
                         }
                         int k = 1;
                         bool end = true;
-                        while (end && i + j + k < c.length() && c[i + j + k] < 0 && k < 6) {
-                            tuple<string, int> e = emojis[c.substr(i, j + k + 1)]; // this all checks if the current emoji found + the next 3-6 charchters
-                            if (!get<0>(e).empty()) {                           // also makes an emoji, if it does then the emoji might be bigger.
+                        while (end && i + j + k < c.length() && c[i + j + k] < 0 && k < 5) {
+                            string e = emojis[c.substr(i, j + k + 1)]; // this all checks if the current emoji found + the next 3-6 charchters
+                            if (!e.empty()) {                           // also makes an emoji, if it does then the emoji might be bigger.
                                 j += k - 1;
                                 end = false;
                             }
                             k += 1;
                         }
                         if (end) { //if the emoji is done then we replace the ascii stuff with the name.
-                            c.replace(i, j + 1, get<0>(e)); //replace the negative chars with ":{emoji name}:"
-                            get<1>(e) += 1;
-                            chars -= (get<0>(e).size() - 1); //accounts for the length of the newly inserted string, then adds 1 because the emoji has 1 length
-                            i += get<0>(e).length() - 1; // moves the pointer to the end of fixed emoji
+                            c.replace(i, j + 1, e); //replace the negative chars with ":{emoji name}:"
+                            chars -= (e.size() - 1); //accounts for the length of the newly inserted string, then adds 1 because the emoji has 1 length
+                            i += e.length() - 1; // moves the pointer to the end of fixed emoji
                             break;
                         }
                     }
@@ -209,17 +201,31 @@ public:
         words.clear();
         int prev = 0;
         int space = c.find(' ');
-        //c.erase(std::remove_if(c.begin(), c.end(), ispunct), c.end());
-        while (space != string::npos) {
-            string str = c.substr(prev, space - prev);
-            if (str != " ")
+        bool done = false;
+        string str;
+        while (!done) { // this code is to add the words composing the content to the words vector
+            if (space == string::npos) {
+                done = true;
+                str = c;
+            }
+            else {
+                str = c.substr(prev, space - prev);
+            }
+            if (!(str[0] == ':' && str[str.length() - 1] == ':')) { // this is to avoid reading the emojis because they're in the form " :[something]: "
+                for (int i = 0; i < str.length(); i++) {
+                    if (str[i] > 0 && ispunct(str[i]))  //this makes it so we don't call ispunct() on any negative chars because that breaks it.
+                        str.erase(i--, 1);
+                }
+            }
+            while (str[0] == 10) {
+                str = str.substr(1);
+            }
+            if (str != " " && str.size() != 0)
                 words.push_back(str);
             c = c.substr(space + 1, string::npos);
             space = c.find(' ');
 
         }
-        words.push_back(c);
-
     }
     float getAvgWordLen() {
         if (avgWordLength == -1) {
@@ -227,7 +233,10 @@ public:
             for (int i = 0; i < words.size(); i++){
                 tot += words[i].length();
             }
-            avgWordLength = tot / words.size();
+            if (words.size() == 0) {
+                return 0;
+            }
+            avgWordLength = (float)tot / (float)words.size();
         }
         return avgWordLength;
     }
@@ -243,6 +252,9 @@ public:
     vector<string> getWords() {
         return words;
     }
+    string getName() {
+        return name;
+    }
 
 
 };
@@ -250,18 +262,86 @@ class User {
 private:
     string name;
     vector<Message> messages;
+    int words;
+    int chars;
+    float avgWords;
+    float avgChars;
+    float avgWordLen;
 public:
+    User() {
+
+    }
     User(string n) {
         name = n;
+        words = -1;
+        chars = -1;
+        avgWords = -1;
+        avgChars = -1;
+        avgWordLen = -1;
+    }
+    void changeMessage(int index, Message m) {
+        messages[index] = m;
     }
     void addMessage(Message m) {
         messages.push_back(m);
+    }
+    int getNumMessage() {
+        return messages.size();
     }
     string getName() {
         return name;
     }
     vector<Message> getMessages() {
         return messages;
+    }
+    float getAvgWordLen() {
+        if (avgWordLen == -1 ) {
+            int sum = 0;
+            for (int i = 0; i < messages.size(); i++) {
+                sum += messages[i].getAvgWordLen();
+            }
+            avgWordLen = (float) sum / (float)messages.size();
+        }
+        return avgWordLen;
+    }
+    float getAvgWords() {
+        if (avgWords == -1) {
+            int sum = 0;
+            for (int i = 0; i < messages.size(); i++) {
+                sum += messages[i].getWords().size();
+            }
+            avgWords = (float)sum / (float)messages.size();
+        }
+        return avgWords;
+    }
+    float getAvgChars() {
+        if (avgChars == -1) {
+            int sum = 0;
+            for (int i = 0; i < messages.size(); i++) {
+ 
+                sum += messages[i].getChars();
+            }
+            avgChars = (float)sum / (float)messages.size();
+        }
+        return avgChars;
+    }
+    int getWords() {
+        if (words == -1) {
+            words++;
+            for (Message m : messages) {
+                words += m.getWords().size();
+            }
+        }
+        return words;
+    }
+    int getChars() {
+        if (chars == -1) {
+            chars++;
+            for (Message m : messages) {
+                chars += m.getChars();
+            }
+        }
+        return chars;
     }
 };
 class Conversation{
@@ -314,18 +394,19 @@ int main() {
     auto begin = std::chrono::high_resolution_clock::now();
     emojis = get_emojis();
     fstream newfile;
-    newfile.open("tpoint.txt", ios::in); //open a file to perform read operation using file object
+    //newfile.open("chat.txt", ios::in); //open a file to perform read operation using file object
+    newfile.open("chat - Copy.txt", ios::in); //open a file to perform read operation using file object
     if (newfile.is_open()) { //checking whether the file is open
         Conversation convo;
         vector<Message> messages;
         vector<User> users;
         string tp;
-        int i = 0;
         string save = "";
         bool sectioned = false;
-        Message m1 = Message(); // this is to hold onto the previous message so we can add content to it if there is multiple lines.
-
+        getline(newfile, tp);
+        int userIndex = 0;
         while (getline(newfile, tp)) { //read data from file object and put it into string.
+
             int first = tp.find_first_of('/');
             int second = tp.find_first_of('/', first + 1);
             int colon = tp.find_first_of(':');
@@ -339,61 +420,84 @@ int main() {
                 d = stoi(tp.substr(first + 1, second - first - 1));
                 y = stoi(tp.substr(second + 1, tp.find_first_of(',') - second - 1));
                 h = stoi(tp.substr(space + 1, colon - space - 1));
-                if (tp[colon + 4] == 'P')
-                    h += 12;
+                if (colon + 4 < tp.length()) {
+                    if (tp[colon + 4] == 'P')
+                        h += 12;
+                }
+                else {
+                    throw invalid_argument("hi");
+                }
                 m = stoi(tp.substr(colon + 1, 2));
             }
             catch (...) { // if the message isn't in the expected form an exception will be thrown, this means that the message has a new line in it
                 sectioned = true; // in this case we want to hold onto this string until there is a correct message
-                save += "\n" + tp;
+                save += " \n" + tp;
                 continue;
             }
             if (sectioned) { //when there is a correct message we add the saved content to the last correct message.
-                m1.addContent(save);
+                messages[messages.size() - 1].addContent(save);
+                users[userIndex].changeMessage(users[userIndex].getNumMessage() - 1, messages[messages.size() - 1]);
                 sectioned = false;
                 save = "";
             }
-            i++;
             string name = tp.substr(dash + 2, colon2 - dash - 2);
             string message = tp.substr(colon2 + 2);
             Time t = Time(m, h, d, mo, y);
-            Message m1 = Message(message, t);
+            Message m1 = Message(name, message, t);
             bool unique = true;
-            for (User u : users) {
-                if (name.compare(u.getName()) == 0) {
-                    u.addMessage(m1);
+            for (int i = 0; i < users.size(); i++) {             
+                if (name.compare(users[i].getName()) == 0) {
+                    users[i].addMessage(m1);
+                    userIndex = i;
                     unique = false;
                 }
             }
             if (unique) {
-                users.push_back(User(name));
-                cout << name;
+                User newbie = User(name);
+                userIndex = users.size();
+                newbie.addMessage(m1);
+                users.push_back(User(newbie));
+               
             }
             //cout << t.toString() + "\n"; 
             messages.push_back(m1);
         }
         //reads line and takes in time, content, and sender
-
+        newfile.close();
         convo = Conversation(users, messages);
         auto end = std::chrono::high_resolution_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
         cout << elapsed.count() * 1e-9;
+        cout << "\n";
        
-        newfile.close();
+
+        for (User u : users) {
+            cout << "name: " + u.getName();
+            cout << "\nnumber of messages: " + to_string(u.getMessages().size());
+            cout << "\nnumber of words: " + to_string(u.getWords());
+            cout << "\nnumber of characters: " + to_string(u.getChars());
+            cout << "\navg num of words: " + to_string(u.getAvgWords());
+            cout << "\navg num of characters: " + to_string(u.getAvgChars());
+            cout << "\navg word length: " + to_string(u.getAvgWordLen());
+            cout << "\n";
+
+        }
+        /*newfile.close();
         fstream outfile;
-        /*outfile.open("dataOut.txt", ios::out);
+        outfile.open("dataOut.txt", ios::out);
         vector<tuple<int, string>> topwords = convo.getTopWords();
         for (int j = 0; j < topwords.size(); j++) {
             tuple<int, string> tup = topwords[j];
             outfile << get<1>(tup) + "  " + to_string(get<0>(tup)) + "\n";
         }
         outfile.close();*/
-        outfile.open("dataOut2.txt", ios::out);
+       /* outfile.open("dataOut2.txt", ios::out);
         for (int j = 0; j < messages.size(); j++) {
             outfile << messages[j].getContent() + "\n";
-        }
+        }*/
         auto end2 = std::chrono::high_resolution_clock::now();
         auto elapsed2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin);
+        cout << "\n";
         cout << elapsed2.count() * 1e-9;
     }   
 };
